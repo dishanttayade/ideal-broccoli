f2d558b66a2b2e299a7fc24a57892c47e2d33457
dishanttayade <dishanttayade1@outlook.com>
2022-05-31 19:33:46 +0530

    localhost changed

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,'html/index.html'));
 })
 
-const link = `http://localhost:${PORT}`;
+const link = `http://moodme-assign.herokuapp.com:${PORT}`;
 
 server.listen(PORT, ()=> {
     console.log(`Go to ${link}`);
